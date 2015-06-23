f31ecae917eed8c96a14c6f3ed3558e9ec962237
jairoji <jairo.ji@hotmail.com>
2015-06-22 21:42:37 -0300

    Código actualizado

--- TP1/Sintaxis/Reglas .R
+++ TP1/Sintaxis/Reglas .R
@@ -47,16 +47,20 @@ datos = read.csv("TP1_DM - Base Consolidada_SINNEGATIVOS.csv", sep = ",", encodi
 datos[["Fecha"]] = as.Date(datos[["Fecha"]], "%d/%m/%Y")
 #datos = na.omit(datos)
 
+datos = subset(datos, Fecha > "2015-01-01")
+#datos = subset(datos, Fecha < "2015-01-01")
 Info.clientes = unique(datos[,c("Venta_ID", "CLI_Gasto", "CLI_Compras", "CLI_CAT_DESC")])
 Info.clientes.bin = as.data.frame(binarize(Info.clientes[,c("CLI_Gasto", "CLI_Compras", "CLI_CAT_DESC")]))
 
-#datos.2014 = subset(datos, Fecha < "2015-01-01")
+
 Producto = datos[,c("Venta_ID", "Prod_ID", "SubCat_Desc", "Cat_Desc", "DescGen")]
 Producto[,"Venta_ID"] = as.factor(Producto[,"Venta_ID"])
 for(i in 1:ncol(Producto))
   Producto[,i] = droplevels(Producto[,i])
 rm(datos)
 Producto = unique(Producto)
+
+##Tablas
 Producto.table = as(as.matrix(as.data.frame.matrix(table(Producto[,c("Venta_ID", "Prod_ID")]))), "transactions")
 SubCat.table = as(as.matrix(as.data.frame.matrix(table(Producto[,c("Venta_ID", "SubCat_Desc")]))), "transactions")
 Cat.table = as(as.matrix(as.data.frame.matrix(table(Producto[,c("Venta_ID", "Cat_Desc")]))), "transactions")
@@ -89,7 +93,7 @@ reglas.DescGen.prunned = data.frame.rules(reglas.DescGen)
 
 rm(reglas.producto, medidas.cat, medidas.SubCat, medidas.DescGen, 
    medidas.Prod, reglas.DescGen, reglas.Cat, reglas.SubCat, 
-   Cat.table, DescGen.table, Producto.table, reglas.producto.subset.prunned)
+   Cat.table, DescGen.table, Producto.table, reglas.producto.subset.prunned, Info.clientes)
 
 write.xlsx(reglas.Cat.prunned, file = "1_ReglasSup0_005.xlsx", sheetName = "Categoría", row.names = F)
 write.xlsx(reglas.SubCat.prunned, file = "1_ReglasSup0_005.xlsx", sheetName = "SubCategoría", row.names = F, append = T)
@@ -132,6 +136,32 @@ write.xlsx(Prod.rules.cliente, "2_Reglas_cliente.xlsx", sheetName = "Producto", 
 write.xlsx(DescGen.rules.cliente, "2_Reglas_cliente.xlsx", sheetName = "Descripción General", row.names = F, append = T)
 write.xlsx(SubCat.rules.cliente, "2_Reglas_cliente.xlsx", sheetName = "Subcategoría", row.names = F, append = T)
 write.xlsx(Cat.rules.cliente, "2_Reglas_cliente.xlsx", sheetName = "Categoría", row.names = F, append = T)
+
+###2014
+write.xlsx(Prod.rules.cliente, "3_1_Reglas_cliente_2014.xlsx", sheetName = "Producto", row.names = F)
+write.xlsx(DescGen.rules.cliente, "3_1_Reglas_cliente_2014.xlsx", sheetName = "Descripción General", row.names = F, append = T)
+write.xlsx(SubCat.rules.cliente, "3_1_Reglas_cliente_2014.xlsx", sheetName = "Subcategoría", row.names = F, append = T)
+write.xlsx(Cat.rules.cliente, "3_1_Reglas_cliente_2014.xlsx", sheetName = "Categoría", row.names = F, append = T)
+
+###2015
+write.xlsx(Prod.rules.cliente, "3_2_Reglas_cliente_2015.xlsx", sheetName = "Producto", row.names = F)
+write.xlsx(DescGen.rules.cliente, "3_2_Reglas_cliente_2015.xlsx", sheetName = "Descripción General", row.names = F, append = T)
+write.xlsx(SubCat.rules.cliente, "3_2_Reglas_cliente_2015.xlsx", sheetName = "Subcategoría", row.names = F, append = T)
+write.xlsx(Cat.rules.cliente, "3_2_Reglas_cliente_2015.xlsx", sheetName = "Categoría", row.names = F, append = T)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 rules.SubCat.table.cliente.prunned = prunning.fun(rules.SubCat.table.cliente)
